d9dde52a5feea644c83cb5b280919add2c8d4cf3
helenabecker <113567226+helenabecker@users.noreply.github.com>
2024-06-22 23:38:35 -0300

    dadosRepertorio

---
+++ src/dados/dadosRepertorio.js
@@ -0,0 +1,226 @@
+const dadosRepertorio = {
+    rockAnos60: [
+      {
+        imagem: '/path/to/livin-on-a-prayer.jpg',
+        nomeMusica: "livin' on a prayer",
+        artista: "Bon Jovi"
+      },
+      {
+        imagem: '/path/to/paradise-city.jpg',
+        nomeMusica: "paradise city",
+        artista: "Guns N' Roses"
+      },
+      {
+        imagem: '/path/to/hells-bells.jpg',
+        nomeMusica: "hells bells",
+        artista: "AC/DC"
+      }
+    ],
+    rockNacional: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    sertanejo: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    baladaAnos80: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    discoMusic: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    oktoberFest: [
+      {
+        imagem: '/path/to/livin-on-a-prayer.jpg',
+        nomeMusica: "livin' on a prayer",
+        artista: "Bon Jovi"
+      },
+      {
+        imagem: '/path/to/paradise-city.jpg',
+        nomeMusica: "paradise city",
+        artista: "Guns N' Roses"
+      },
+      {
+        imagem: '/path/to/hells-bells.jpg',
+        nomeMusica: "hells bells",
+        artista: "AC/DC"
+      }
+    ],
+    rockInternacional: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    pagode: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    musicaGaucha: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    forro: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    funk: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    reggae: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ],
+    popRockNacional: [
+        {
+            imagem: '/path/to/livin-on-a-prayer.jpg',
+            nomeMusica: "livin' on a prayer",
+            artista: "Bon Jovi"
+          },
+          {
+            imagem: '/path/to/paradise-city.jpg',
+            nomeMusica: "paradise city",
+            artista: "Guns N' Roses"
+          },
+          {
+            imagem: '/path/to/hells-bells.jpg',
+            nomeMusica: "hells bells",
+            artista: "AC/DC"
+          }
+    ]
+  };
+  
+  export default dadosRepertorio;
+  
