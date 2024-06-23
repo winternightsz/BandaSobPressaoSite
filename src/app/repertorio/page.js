b93073da64d5cd3f2b5010e4443fc86c5a7e5e46
helenabecker <113567226+helenabecker@users.noreply.github.com>
2024-06-22 23:40:18 -0300

    page repertorio

--- src/app/repertorio/page.js
+++ src/app/repertorio/page.js
@@ -1,17 +1,187 @@
-export const metadata = {
-  title: "Repertório",
-  description: "Site da banda Sob Pressão",
+import React from 'react';
+import CardMusica from '@/components/CardMusica';
+import dadosRepertorio from '@/dados/dadosRepertorio';
+import { PlayIcon } from '@heroicons/react/solid';
+
+
+const Repertorio = () => {
+  const pegarMusicas = (musicas) => {
+    return musicas.map((musica, index) => (
+      <div key={index} className="max-w-xs mx-0 ">
+        <CardMusica
+          imagem={musica.imagem}
+          nomeMusica={musica.nomeMusica}
+          artista={musica.artista}
+        />
+      </div>
+    ));
+  };
+
+  return (
+    <div className="flex justify-center flex-col items-center min-h-screen bg-blue-600 text-white">
+      <div className="text-center mb-10">
+        <div className="text-center mb-10 bg-opacity-70 bg-blue-600" style={{ backgroundImage: 'url("/imagens/foto-vinil-repertorio.png")' }}>
+          <h1 className="text-4xl font-bold">REPERTÓRIO</h1>
+          <p>Clique no play e escute a playlist para cada gênero no Spotify!</p>
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulPrincipal to-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Rock dos Anos 60
+          </a>
+        </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.rockAnos60)}
+        </div>
+      </div>
+
+      <div className="bg-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Rock Nacional
+            </a>
+        </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.rockNacional)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulEmBaixo to-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Rock Internacional
+          </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.rockInternacional)}
+        </div>
+      </div>
+     
+      <div className="bg-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Balada dos Anos 80
+          </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.baladaAnos80)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulPrincipal to-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Disco Music
+          </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.discoMusic)}
+        </div>
+      </div>
+
+      <div className="bg-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+          Pop Rock Nacional
+          </a>
+        </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.popRockNacional)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulEmBaixo to-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Sertanejo
+          </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.sertanejo)}
+        </div>
+      </div>
+
+      <div className="bg-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Pagode
+          </a></h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.pagode)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulPrincipal to-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Música Gaúcha
+          </a>
+        </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.musicaGaucha)}
+        </div>
+      </div>
+
+      <div className="bg-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Forró
+            </a>
+         </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.forro)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulEmBaixo to-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Funk
+            </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.funk)}
+        </div>
+      </div>
+
+      <div className="bg-azulPrincipal w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            OktoberFest
+            </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.oktoberFest)}
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-b from-azulPrincipal to-azulEmBaixo w-full py-10">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          <a href="#" className="flex items-center justify-center text-white hover:text-gray-200">
+            <PlayIcon className="h-8 w-8 mr-2" /> {/* Ícone de play */}
+            Reggae
+            </a>
+          </h2>
+        <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {pegarMusicas(dadosRepertorio.reggae)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-
-const RepertorioPage = () => {
-    return (
-      <div>
-        <h2>Repertório</h2>
-        <p>Esta é a página de repertório da banda.</p>
-      </div>
-    );
-  };
-  
-  export default RepertorioPage;
-  
+export default Repertorio;
