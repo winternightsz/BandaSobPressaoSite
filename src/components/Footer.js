09927adb82de8e7c1ec7aff802ffc7f2d8a6a7fe
helenabecker <113567226+helenabecker@users.noreply.github.com>
2024-06-23 11:30:47 -0300

    footer centralizado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-white">
-      <div className="flex justify-between items-center px-4">
-        <div className="flex items-center">
+    <footer className="bg-white text-white pb-1 px-2">
+      <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+        <div className="flex items-center justify-center md:justify-start flex-1">
           <Image
             src="/imagens/logoBandaColorida.png" 
             alt="Logo da Banda"
@@ -14,18 +14,19 @@ const Footer = () => {
           />
         </div>
 
-        <div className="text-black text-center">
+        <div className="flex-1 text-black text-center">
           <p>&copy; 2024 My Band. Todos os direitos reservados.</p>
         </div>
 
-        <div>
-          <ul className="flex space-x-4">
+        <div className="flex-1 flex justify-center md:justify-end w-full md:w-auto">
+          <ul className="flex flex-wrap justify-center space-x-4">
             <li><a href="#">
               <Image
                 src="/imagens/itens_footer/instagram.png"
                 alt="ícone do Instagram"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
             <li><a href="#">
@@ -34,6 +35,7 @@ const Footer = () => {
                 alt="ícone do Facebook"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
             <li><a href="#">
@@ -42,6 +44,7 @@ const Footer = () => {
                 alt="ícone do WhatsApp"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
             <li><a href="#">
@@ -50,6 +53,7 @@ const Footer = () => {
                 alt="ícone do Gmail"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
             <li><a href="#">
@@ -58,6 +62,7 @@ const Footer = () => {
                 alt="ícone do Youtube"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
             <li><a href="#">
@@ -66,6 +71,7 @@ const Footer = () => {
                 alt="ícone do Spotify"
                 width={30}
                 height={30}
+                className="w-8 h-8"
               />
             </a></li>
           </ul>
